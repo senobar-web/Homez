d8495ac0534a12a24ed8d599f59dc62e12d90edb
asal <assal.senobarnya@gmail.com>
2025-09-10 12:21:49 +0330

    creating cheapestHouses.type.ts file from home template

---
+++ src/components/template/Home/CheapestHouses.type.ts
@@ -0,0 +1,12 @@
+export type CheapestHousesItem ={
+    id:number,
+    title:string,
+    city:string,
+    price:number,
+    room:number,
+    bathroom:number,
+    meterage:number,
+    status:string,
+    img:string,
+    category:string,
+}
