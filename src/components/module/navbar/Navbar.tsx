09d34f068132cfc8c1c718f039bd1322aaedf400
asal <assal.senobarnya@gmail.com>
2025-09-10 11:30:33 +0330

    creating navbar component  modules

---
+++ src/components/module/navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import type { JSX } from "react";
+// import { IoMdArrowDropdown } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { Dropdown, DropdownItem } from "flowbite-react";
+
+export default function Navbar(): JSX.Element {
+  return (
+    <>
+      <nav className="flex gap-2 items-center justify-start ">
+        <div className="py-3 px-5 rounded-4xl hover:bg-gray-900 flex justify-center items-center text-white cursor-pointer ">
+          <Link to="/" className="text-sm font-bold  ">
+            صفحه اصلی
+          </Link>
+        </div>
+        <div className="py-3 px-5 rounded-4xl hover:bg-gray-900  flex justify-center items-center text-white cursor-pointer">
+          <h3 className=" text-sm font-bold "> فهرست ها </h3>
+        </div>
+        <div className="py-3 px-5 rounded-4xl hover:bg-gray-900  flex justify-center text-white items-center cursor-pointer">
+          <h3 className=" text-sm font-bold "> اعضا</h3>
+        </div>
+        <div className="py-3 px-5 rounded-4xl hover:bg-gray-900  flex justify-center items-center text-white cursor-pointer">
+          <h3 className=" text-sm font-bold "> وبلاگ</h3>
+        </div>
+        {/* <div className=" relative py-2 px-8 rounded-4xl  hover:bg-gray-900  flex justify-center items-center text-white cursor-pointer "> */}
+          {/* <h3 className="text-white text-sm font-bold "> صفحات</h3>
+          <IoMdArrowDropdown className="w-5 h-5" />
+          <ul className="opacity-0 invisible bg-white group-hover:opacity-100 group-hover:visible rounded-xl text-black w-[250px] absolute overflow-hidden top-12 shadow-2xl z-[9999] ">
+            <li className="hover:bg-red-50 px-4 py-2 text-gray-700">
+              <Link to="/contact">تماس با ما</Link>
+            </li>
+            <li className="hover:bg-red-50 px-4 py-2 text-gray-700">
+              <Link to="/about"> درباره .ما</Link>
+            </li>
+            <li className="hover:bg-red-50 px-4 py-2 text-gray-700">
+              <Link to="/non-existent-path"> 404</Link>
+            </li>
+            <li className="hover:bg-red-50 px-4 py-2  text-gray-700">
+              <Link to="/login"> ورود / ثبت نام</Link>
+            </li>
+          </ul> */}
+          <Dropdown label="صفحات" >
+              <DropdownItem as={Link} to="/contact" >
+                تماس با ما
+              </DropdownItem>
+              <DropdownItem as={Link} to="/about" >
+                درباره ما
+              </DropdownItem>
+              <DropdownItem as={Link} to="/login" >
+                ورود / ثبت نام
+              </DropdownItem>
+              <DropdownItem as={Link} to="/non-existent-path">
+                ۴۰۴
+              </DropdownItem>
+          </Dropdown>
+        {/* </div> */}
+      </nav>
+    </>
+  );
+}
