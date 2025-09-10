8649c1a2175ac51a61a0e8b534c00e061ed42670
asal <assal.senobarnya@gmail.com>
2025-09-10 11:35:12 +0330

    creating details component from pages

---
+++ src/components/pages/detailes/Details.tsx
@@ -0,0 +1,21 @@
+import Footer from "../../module/Footer/Footer";
+import TopHeader from "../../module/TopHeader/TopHeader";
+import Property from "../../template/details/Property";
+import AdditionalDetails from "../../template/details/AdditionalDetails";
+
+
+export default function Details() {
+  
+  return (
+    <div className="bg-gray-50">
+      <TopHeader />
+      <div className="container text-[#181A20] ">
+        <Property/>
+        <section className="mt-10">
+          <AdditionalDetails/>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+}
