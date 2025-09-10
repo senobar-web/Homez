bf49cf4e69a55716ce1b924cbe5bbaccd20726af
asal <assal.senobarnya@gmail.com>
2025-09-10 11:22:52 +0330

    creating contact component from contact  modules

---
+++ src/components/module/Contact/Contact.tsx
@@ -0,0 +1,16 @@
+// import Title from "../Title/Title";
+// import { titlesItem } from "../../../data";
+import "aos/dist/aos.css";
+import Logo from "./Logo";
+import MoreInfo from "./MoreInfo";
+
+export default function Contact() {
+  return (
+    <>
+      <section className="lg:mb-20">
+        <Logo />
+        <MoreInfo />
+      </section>
+    </>
+  );
+}
