263ace3afbd67fe6592895a6e6e0bfe4827c04bd
asal <assal.senobarnya@gmail.com>
2025-09-10 10:28:23 +0330

    adding context and router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import routes from "../routes.tsx";
+import { useRoutes } from "react-router-dom";
+import ContextItemsProvider from "./components/module/Context/ItemsContext.tsx";
+import { AuthProvider } from "./components/module/Context/AuthUser.tsx";
 
 function App() {
-  return <></>;
+  const router = useRoutes(routes);
+
+  return (
+    <AuthProvider>
+      <ContextItemsProvider>
+        {router}
+      </ContextItemsProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
