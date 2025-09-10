ef6e93f3ccd94d6832d4dc39b35e2de3a2fe4e40
asal <assal.senobarnya@gmail.com>
2025-09-10 11:47:30 +0330

    creating sidebar.type.ts  from property pages

---
+++ src/components/pages/property/Sidebar.type.ts
@@ -0,0 +1,8 @@
+ export type resSliderProps = {
+  onFilterChange:(newFilters: any) => void ,
+  priceRange: number[];
+  selectedCity: string;
+  selectedRoom: null;
+  selectedOptionCheck: string[];
+  selectedOption: string;
+};
