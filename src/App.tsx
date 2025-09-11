eeb44d43520507e2706368bcb0a0d7b81a658e74
asal <assal.senobarnya@gmail.com>
2025-09-11 19:45:27 +0330

    Remove Auhprovider context from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import routes from "../routes.tsx";
 import { useRoutes } from "react-router-dom";
 import ContextItemsProvider from "./components/module/Context/ItemsContext.tsx";
-import { AuthProvider } from "./components/module/Context/AuthUser.tsx";
 
 function App() {
   const router = useRoutes(routes);
 
-  return (
-    <AuthProvider>
-      <ContextItemsProvider>
-        {router}
-      </ContextItemsProvider>
-    </AuthProvider>
-  );
+  return <ContextItemsProvider>{router}</ContextItemsProvider>;
 }
 
 export default App;
