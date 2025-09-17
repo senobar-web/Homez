8a3c619540f29e4e08f49bab3caea2d2647ebae2
asal <assal.senobarnya@gmail.com>
2025-09-17 09:34:15 +0330

    add API_URL variable to blog component template

--- src/components/template/Home/Blog.tsx
+++ src/components/template/Home/Blog.tsx
@@ -6,31 +6,36 @@ import { Autoplay, Navigation } from "swiper/modules";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
+import { Api_Url } from "../../module/Api_url/API";
 export default function Blog() {
-  const[blogsItems,setBlogsItems]=useState<Blogs[]>([])
+  const [blogsItems, setBlogsItems] = useState<Blogs[]>([]);
   useEffect(() => {
-      AOS.init({
-        duration: 1000, // Animation duration
-        easing: "ease-in-out", // Animation easing
-      });
-    }, []);
-    useEffect(()=>{
-      axios.get("http://localhost:5000/BlogsItems").then(res=>{
-        setBlogsItems(res.data)
-      })
-    },[])
+    AOS.init({
+      duration: 1000, // Animation duration
+      easing: "ease-in-out", // Animation easing
+    });
+  }, []);
+  useEffect(() => {
+    axios.get(`${Api_Url}/BlogsItems`).then((res) => {
+      setBlogsItems(res.data);
+    });
+  }, []);
   return (
     <>
       <section className="my-9 py-14 ">
         <div className="container">
-          <div className="text-center" data-aos='fade-up'>
+          <div className="text-center" data-aos="fade-up">
             <h3 className="text-[#181A20] text-4xl font-bold">وبلاگ ما</h3>
             <p className="text-[#717171] text-sm/12">
               {" "}
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم است
             </p>
           </div>
-          <div className="flex justify-between mt-8 " data-aos='fade-up' data-aos-delay="100">
+          <div
+            className="flex justify-between mt-8 "
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
             <Swiper
               className="mySwiper h-auto "
               loop={true}
