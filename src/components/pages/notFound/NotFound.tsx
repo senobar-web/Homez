62e6c5d145f70fee62c6b47fce90bfe9bd962019
asal <assal.senobarnya@gmail.com>
2025-09-17 09:54:11 +0330

    add lazyloading property to notFound component page

--- src/components/pages/notFound/NotFound.tsx
+++ src/components/pages/notFound/NotFound.tsx
@@ -8,11 +8,11 @@ export default function NotFound() {
       <TopHeader />
       <div className="container my-24">
         <div className="flex flex-col md:flex-row  justify-between items-center gap-x-3 gap-y-14 ">
-          <div >
-            <img src="/img/error.jpg" alt="error" />
+          <div>
+            <img src="/img/error.jpg" alt="error" loading="lazy" />
           </div>
           <div>
-            <img src="/img/icon-error.png" alt="" />
+            <img src="/img/icon-error.png" alt="" loading="lazy" />
             <div className="text-black mb-10 mt-10">
               <h2 className="font-bold text-4xl my-10 ">
                 متاسفانه صفحه مورد نظر شما پیدا نشد
