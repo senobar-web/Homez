8f0f1ad656c4928bf97b6352284810e523ad8a42
asal <assal.senobarnya@gmail.com>
2025-09-24 22:36:26 +0330

    applaying ESLint & prettier rules & specify type to StatusBtn.tsx file from home template

--- src/components/template/Home/StatusBtn.tsx
+++ src/components/template/Home/StatusBtn.tsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
-
-export default function StatusBtn({statuses,filterItem }) {
-  const [mainCategory, setMainCategory] = useState("برای فروش");
+import {useState} from 'react';
+interface StatusBtnProps {
+  statuses: string[];
+  filterItem: (status: string) => void;
+}
+export default function StatusBtn(statusButton: StatusBtnProps) {
+  const [mainCategory, setMainCategory] = useState('برای فروش');
   return (
     <>
-      {statuses.map((status:string, index:number) => (
+      {statusButton.statuses.map((status: string, index: number) => (
         <button
           type="button"
           className={
-            status === mainCategory ? "h-full flex items-center justify-center text-[#181A20] bg-white rounded-lg text-sm font-bold py-2 px-4  cursor-pointer" : " h-full flex items-center justify-center rounded-lg bg-[#FFFFFF14] text-white text-sm font-bold py-2 px-4  cursor-pointer"
+            status === mainCategory
+              ? 'h-full flex items-center justify-center text-[#181A20] bg-white rounded-lg text-sm font-bold py-2 px-4  cursor-pointer'
+              : ' h-full flex items-center justify-center rounded-lg bg-[#FFFFFF14] text-white text-sm font-bold py-2 px-4  cursor-pointer'
           }
           key={index}
-          onClick={()=>{
-            setMainCategory(status)
-            filterItem(status)
+          onClick={() => {
+            setMainCategory(status);
+            statusButton.filterItem(status);
           }}
         >
           {status}
