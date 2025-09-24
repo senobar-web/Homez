2bbeeb75294322176492fab2c750937d6fc5dd22
asal <assal.senobarnya@gmail.com>
2025-09-24 23:06:30 +0330

    applaying ESLint & prettier rules to from about page

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,7 +1,7 @@
-import Contact from "../../module/Contact/Contact";
-import Footer from "../../module/Footer/Footer";
-import IndexAbout from "../../template/About/IndexAbout";
-import TopHeader from "../../module/TopHeader/TopHeader";
+import Contact from '../../module/Contact/Contact';
+import Footer from '../../module/Footer/Footer';
+import IndexAbout from '../../template/About/IndexAbout';
+import TopHeader from '../../module/TopHeader/TopHeader';
 
 export default function About() {
   return (
