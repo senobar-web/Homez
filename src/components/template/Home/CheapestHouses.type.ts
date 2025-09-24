25ba143ce7904a2e2cec2ffc4881ef4d45bf5109
asal <assal.senobarnya@gmail.com>
2025-09-24 22:29:33 +0330

    applaying ESLint & prettier rules to CheapestHouses.type file from home template

--- src/components/template/Home/CheapestHouses.type.ts
+++ src/components/template/Home/CheapestHouses.type.ts
@@ -1,13 +1,13 @@
-export type CheapestHousesItem ={
-    id:number,
-    title:string,
-    city:string,
-    price:number,
-    room:number,
-    bathroom:number,
-    meterage:number,
-    status:string,
-    img:string,
-    category:string,
-    citycenter:string
-}
+export type CheapestHousesItem = {
+  id: number;
+  title: string;
+  city: string;
+  price: number;
+  room: number;
+  bathroom: number;
+  meterage: number;
+  status: string;
+  img: string;
+  category: string;
+  citycenter: string;
+};
