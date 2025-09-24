8866752f0076abdb3e4d68858d44e243c4e813d4
asal <assal.senobarnya@gmail.com>
2025-09-24 23:47:12 +0330

    applaying ESLint & prettier rules & modify function parameter from ApartmentItems module

--- src/components/module/ApartmentItems/Apartment.type.ts
+++ src/components/module/ApartmentItems/Apartment.type.ts
@@ -1,8 +1,8 @@
-import type { JSX } from "react";
+import type {JSX} from 'react';
 
-export type ApartmentTypes= {
-    id:number,
-    icon:JSX.Element, 
-    count:number,
-    title:string,    
-}
+export type ApartmentTypes = {
+  id: number;
+  icon: JSX.Element;
+  count: number;
+  title: string;
+};
--- src/components/module/ApartmentItems/ApartmentItem.tsx
+++ src/components/module/ApartmentItems/ApartmentItem.tsx
@@ -1,31 +1,30 @@
-import type { ApartmentTypes } from "./Apartment.type";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import type {ApartmentTypes} from './Apartment.type';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import {useEffect} from 'react';
+import {Link} from 'react-router-dom';
 
-export default function ApartmentItem({title, count, icon }: ApartmentTypes) {
+export default function ApartmentItem(elements: ApartmentTypes) {
   useEffect(() => {
     AOS.init({
       duration: 800, // Animation duration
-      easing: "ease-in-out", // Animation easing
+      easing: 'ease-in-out', // Animation easing
     });
   }, []);
   return (
     <>
       <Link
-        to={`/property-type/${title}`}
-        // to={`/property-type/${id}`}
+        to={`/property-type/${elements.title}`}
         className="block p-7 bg-gray-100 hover:bg-gray-950 rounded-2xl  h-full group "
         data-aos="fade-up"
       >
         <div>
           <div className="w-16 h-16 rounded-full bg-white flex items-center justify-center group-hover:bg-[#ffffff14]">
-            {icon}
+            {elements.icon}
           </div>
           <div className="text-gray-950 mt-12 group-hover:text-gray-200">
-            <h3 className="text-xl font-bold "> {title}</h3>
-            <p className="text-sm/9 ">{count} ملک </p>
+            <h3 className="text-xl font-bold "> {elements.title}</h3>
+            <p className="text-sm/9 ">{elements.count} ملک </p>
           </div>
         </div>
       </Link>
