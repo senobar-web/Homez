7093f5ac3fa0542eddac5da59fd916ff79eeb2b7
asal <assal.senobarnya@gmail.com>
2025-11-15 16:52:23 +0330

    refactore newsLetter components

--- src/components/template/Footer/Newsletter.tsx
+++ src/components/template/Footer/Newsletter.tsx
@@ -1,58 +1,42 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import ApiRequest from '../../module/Api_url/ApiRequest';
-import type {ApiResponse} from '../../module/Api_url/ApiRequest';
-import {useMutation} from '@tanstack/react-query';
+import {useFormMutation} from '../../../hooks/useForm';
 
-interface SubscribeResponse {
+interface FormInput {
   email: string;
 }
 export default function Newsletter() {
-  const [email, setEmail] = useState('');
-  const mutation = useMutation<ApiResponse<SubscribeResponse>, Error, string>({
-    mutationFn: async (email: string) => {
-      const newEmail = {email};
-      const response = await ApiRequest('/newsletter', 'POST', newEmail);
-      return response;
-    },
+  const {register, handleSubmit, errors, mutation, onSubmit} = useFormMutation<FormInput>({
+    endpoint: '/newsletter',
+    successMessage: 'ایمیل با موفقیت ثبت شد!',
+    defaultValues: {email: ''},
   });
-
-  const handelSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    mutation.mutate(email);
-  };
-  useEffect(() => {
-    if (mutation.isSuccess) {
-      alert('ایمیل با موفقیت ارسال شد');
-      setEmail('');
-    }
-    if (mutation.isError) {
-      alert('Error sending: ' + (mutation.error as Error).message);
-    }
-  }, [mutation.isSuccess, mutation.isError]);
-
   return (
     <div>
       <div>
         <h3>اخبار روز را دنبال کنید</h3>
-        <form onSubmit={handelSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="relative ">
             <input
               type="email"
               id="search"
               className="mt-7 block w-full  p-6 ps-10 text-sm  text-white  rounded-lg bg-gray-800 "
               placeholder="ایمیل خود را بنویسید"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register('email', {
+                required: 'ایمیل الزامی است',
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: 'ایمیل وارد شده معتبر نیست',
+                },
+              })}
             />
             <button
               type="submit"
-              className="hover:cursor-pointer text-white absolute end-2.5 bottom-2.5  hover:text-red-dark  font-medium  text-sm px-6 py-4    "
+              disabled={mutation.isPending}
+              className={`hover:cursor-pointer  text-white absolute end-2.5 bottom-2.5  focus:border-none hover:text-red-dark  font-medium  text-sm px-6 py-4 `}
             >
-              عضویت !
+              {mutation.isPending ? 'در حال ارسال...' : 'عضویت!'}
             </button>
           </div>
+          {errors.email && <p className="text-red-400 text-sm mt-2">{errors.email.message}</p>}
         </form>
       </div>
     </div>
